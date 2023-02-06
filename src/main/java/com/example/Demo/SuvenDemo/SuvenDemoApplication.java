4d37ced55c0dbd42db7d7efbe34ab65a36e2e008
sumanCap <suman.a.paul@capgemini.com>
2023-02-07 01:10:34 +0530

    my second commit

---
+++ src/main/java/com/example/Demo/SuvenDemo/SuvenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Demo.SuvenDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuvenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuvenDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Demo/SuvenDemo/SuvenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Demo.SuvenDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuvenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
